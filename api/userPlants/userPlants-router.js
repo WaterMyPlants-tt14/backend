e1a9ddab35bffd05af98a45fc9818edd590669be
ClintFix <clint.fix@gmail.com>
2021-04-24 15:08:14 -0600

    get user plants by id working.

--- api/userPlants/userPlants-router.js
+++ api/userPlants/userPlants-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userPlants = require("./userPlants-model");
 
 router.get("/", (req, res, next) => {
-    userPlants.all()
+    userPlants.findById(id)
         .then(plants => res.status(200).json(plants))
         .catch(next);
 });
