3c2f67c0dfc6fdaa1e6ccf750341577d034763dc
ClintFix <clint.fix@gmail.com>
2021-04-24 14:45:35 -0600

    species model and router done

--- api/server.js
+++ api/server.js
@@ -3,9 +3,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const server = express();
 
+const speciesRouter = require('./species/species-router');
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use('/api/species', speciesRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
