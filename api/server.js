69947bc1f2afb2f485dac8d041e16178ca5d24d3
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-26 13:26:12 -0700

    restricted middleware and auth routes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const server = express();
 
 const speciesRouter = require('./species/species-router');
 const userPlantsRouter = require('./userPlants/userPlants-router');
+const authRouter = require('./auth/auth-router.js')
 
 server.use(express.json());
 server.use(helmet());
@@ -12,12 +13,13 @@ server.use(cors());
 
 server.use('/api/species', speciesRouter);
 server.use('/api/userplants', userPlantsRouter);
+server.use('/api/auth', authRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
-    res.status(err.status || 500).json({
-      message: err.message,
-      stack: err.stack,
-    });
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 module.exports = server;
