7599aa70fc346e31bd8407eb86b124949d43a785
ClintFix <clint.fix@gmail.com>
2021-04-27 10:29:25 -0600

    fixed middleware

--- api/userPlants/userPlants-model.js
+++ api/userPlants/userPlants-model.js
@@ -13,7 +13,7 @@ function findUserPlantsByPlantsID(user_plant_id){
         .join("species as sp", "up.species_id", "sp.species_id")
         .join("water_schedule as ws", "sp.water_id", "ws.water_id")
         .where("up.user_plant_id", user_plant_id)
-        .select("up.user_plant_id", "up.plant_nickname", "up.plant_location", "up.notes", "up.water_day", "sp.species_id", "sp.plant_name", "sp.plant_scientific_name", "sp.plant_image", "ws.water_schedule")
+        .select("up.user_plant_id", "up.plant_nickname", "up.plant_location", "up.notes", "up.water_day", "sp.species_id", "sp.plant_name", "sp.plant_scientific_name", "sp.plant_image", "ws.water_schedule", "up.user_id")
         .first();
 }
 
