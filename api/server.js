5dea8729b52aa1561d7b3be43de0b9e9faf85187
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-04-27 11:20:38 -0400

    made the restriced a little better

--- api/server.js
+++ api/server.js
@@ -7,16 +7,16 @@ const speciesRouter = require('./species/species-router');
 const userPlantsRouter = require('./userPlants/userPlants-router');
 const authRouter = require('./auth/auth-router.js');
 const userRouter = require('./users/users-router');
-const restricted = require('../middleware/restricted');
+const restricted = require('./middleware/restricted');
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
 server.use('/api/species', speciesRouter);
-server.use('/api/userplants', userPlantsRouter);
+server.use('/api/userplants',restricted, userPlantsRouter);
 server.use('/api/users', userRouter);
-server.use('/api/auth',restricted, authRouter);
+server.use('/api/auth', authRouter);
 
 
 
